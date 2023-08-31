93013a494a495b06d3f0fda59b96e5ca0360ebab
tanziti <tanzitian3@gmail.com>
2023-08-31 17:01:14 -0400

    step 10

--- art-museum/src/components/GalleryNavigation/index.jsx
+++ art-museum/src/components/GalleryNavigation/index.jsx
@@ -2,10 +2,16 @@ import { NavLink } from "react-router-dom";
 
 const GalleryNavigation = ({ galleries }) => {
     console.log(galleries)
+    const galleriesList = galleries.map((gallery) => {
+        return <NavLink to={`/galleries/${gallery.id}`}>
+            {gallery.name}
+        </NavLink>
+    })
     return (
         <nav>
             <h1>Galleries</h1>
             <NavLink to={`/`}>Home Page</NavLink>
+            {galleriesList}
         </nav>
     )
 }
